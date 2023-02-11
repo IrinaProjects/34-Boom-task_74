a3119bb170d6593327008e5c811d0052da5f9bc5
Irina Spasova <irina.spasova.81@gmail.com>
2023-02-11 18:37:35 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import "./App.css";
+import { useState, useMemo } from "react";
+
+
+function isNumber(str) {
+  const checkNum = new RegExp('^[0-9\b]+$');
+  if (checkNum.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 
 function App() {
+  const [text, setNum] = useState('');
+
+
+
+  const checkNum = useMemo(() => isNumber(text), [text]);
+
+  const handleChange = event => {
+    setNum(event.target.value);
+  };
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          onChange={handleChange}
+          value={text}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={checkNum ? "fas fa-check" : "fas fa-times"} />
+        </span>
       </div>
     </div>
   );
